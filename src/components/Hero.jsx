136b64a784a921b68766698c620c21bca16b72f1
Henok <henokaragaw17@gmail.com>
2024-10-15 09:17:59 -0900

    Your commit message here

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,4 +1,4 @@
-import { NavLink } from "react-router-dom"
+
 import { HeroContainer } from "../assets/Images/images"
 import { statistics } from "../constants/constant"
 
@@ -13,9 +13,9 @@ const Hero = () => {
          <div className="w-full flex flex-col xl:w-4/5">
           <h1 className="font-Urbanist font-semibold dark:text-white xl:text-[55px] sm:text-[40px] md:text-[45px] max-sm:text-[35px]">Discover Your Dream Property with Estatein</h1>
           <p className="max-w-lg font-Urbanist dark:text-[#999999] font-medium leading-[150%] py-8">Your journey to finding the perfect property begins here. Explore our listings to find the home that matches your dreams.</p>
-          <div className="flex max-sm:flex-col gap-4 max-sm:w-full md:flex-row">
-             <button className="border border-[#262626] rounded-[8px] px-2 py-3 dark:text-white font-Urbanist font-medium text-[14px]">Learn More</button>
-             <NavLink><button className="bg-[#703BF7] dark:text-white rounded-[8px] px-2 py-3">Browse Properties</button></NavLink>
+          <div className="flex items-center max-sm:flex-col gap-4 md:flex-row">
+             <button className="border border-[#262626] rounded-[8px] px-2 py-3 dark:text-white font-Urbanist font-medium text-[14px] w-full md:w-56">Learn More</button>
+             <button className="bg-[#703BF7] dark:text-white rounded-[8px] px-2 py-3 w-full md:w-56">Browse Properties</button>
           </div>
           <div className="grid max-md:grid-cols-2 md:grid-cols-3 gap-6 my-10 w-full">
             {statistics.map((statistic) => (
